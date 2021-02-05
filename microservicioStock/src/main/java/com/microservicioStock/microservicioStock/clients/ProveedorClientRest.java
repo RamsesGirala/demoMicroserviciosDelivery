088b62159f3401548651ece022d6c01d85b1fae5
rodrigogcespedes <rodrigogcespedes@gmil.com>
2021-02-05 17:28:25 -0300

    posts y puts

--- microservicioStock/src/main/java/com/microservicioStock/microservicioStock/clients/ProveedorClientRest.java
+++ microservicioStock/src/main/java/com/microservicioStock/microservicioStock/clients/ProveedorClientRest.java
@@ -1,5 +1,6 @@
 package com.microservicioStock.microservicioStock.clients;
 
+import com.microservicioStock.microservicioStock.models.Existencia;
 import com.microservicioStock.microservicioStock.models.Proveedor;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
@@ -9,15 +10,20 @@ import org.springframework.web.bind.annotation.*;
 public interface ProveedorClientRest {
 
     @GetMapping("/api/v1/proveedor/{id}")
-    public ResponseEntity<?> getOne(@PathVariable Long id);
+    public ResponseEntity<?> getOneProveedor(@PathVariable Long id);
 
     @PostMapping("/api/v1/proveedor/")
-    public ResponseEntity<?> save(@RequestBody Proveedor entity);
+    public ResponseEntity<?> saveProveedor(@RequestBody Proveedor entity);
 
     @PutMapping("/api/v1/proveedor/{id}")
-    public ResponseEntity<?> update(@PathVariable Long id,@RequestBody Proveedor entity);
+    public ResponseEntity<?> updateProveedor(@PathVariable Long id,@RequestBody Proveedor entity);
 
     @GetMapping("/api/v1/existencia/{id}")
-    public ResponseEntity<?> getOneE(@PathVariable Long id);
+    public ResponseEntity<?> getOneExistencia(@PathVariable Long id);
 
+    @PostMapping("/api/v1/existencia/")
+    public ResponseEntity<?> saveExistencia(@RequestBody Existencia entity);
+
+    @PutMapping("/api/v1/existencia/{id}")
+    public ResponseEntity<?> updateExistencia(@PathVariable Long id,@RequestBody Existencia entity);
 }
--- microservicioStock/src/main/java/com/microservicioStock/microservicioStock/controllers/InsumoController.java
+++ microservicioStock/src/main/java/com/microservicioStock/microservicioStock/controllers/InsumoController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping(path = "api/v1/insumo")
-public class InsumoController extends BaseControllerImpl<Insumo, InsumoServiceImpl>{
+public class InsumoController extends BaseControllerImpl<Insumo, InsumoServiceImpl> {
 
     @Autowired
     private ProveedorClientRest provClient;
@@ -25,87 +25,111 @@ public class InsumoController extends BaseControllerImpl<Insumo, InsumoServiceIm
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getOne(@PathVariable Long id) {
-        try{
+        try {
             //este es el buscar
             Insumo i = service.findById(id);
 
             //para XtoOne
-            Proveedor prov = gson.fromJson(provClient.getOne(i.getIdDistribProv()).getBody().toString(),Proveedor.class);
-
+            Proveedor prov = gson.fromJson(provClient.getOneProveedor(i.getIdDistribProv()).getBody().toString(), Proveedor.class);
             i.setProveedor(prov);
 
             //para XtoMany
-            for(Articulos_Existencia a : i.getArticulos_existencias()){
-
-                a.setExistencia(gson.fromJson(provClient.getOneE(a.getId_existencia()).getBody().toString(), Existencia.class));
-
-            }
+            for (Articulos_Existencia a : i.getArticulos_existencias())
+                a.setExistencia(gson.fromJson(provClient.getOneExistencia(a.getId_existencia()).getBody().toString(), Existencia.class));
 
             return ResponseEntity.status(HttpStatus.OK).body(i);
 
-        }catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Error\"}");
         }
     }
 
     @PostMapping("")
     public ResponseEntity<?> save(@RequestBody Insumo entity) {
-        try{
-
-            Proveedor prov = gson.fromJson(provClient.getOne(entity.getIdDistribProv()).getBody().toString(),Proveedor.class);
-
+        try {
+            // para XtoOne
+            Proveedor prov = gson.fromJson(provClient.getOneProveedor(entity.getIdDistribProv()).getBody().toString(), Proveedor.class);
             entity.setProveedor(prov);
 
+            // para XtoMany
+            for (Articulos_Existencia a : entity.getArticulos_existencias())
+                a.setExistencia(gson.fromJson(provClient.getOneExistencia(a.getId_existencia()).getBody().toString(), Existencia.class));
+
             return ResponseEntity.status(HttpStatus.OK).body(service.save(entity));
-        }catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error\"}");
         }
     }
 
     @PostMapping("/subordinada")
     public ResponseEntity<?> saveSubordinada(@RequestBody Insumo entity) {
-        try{
-
-            Proveedor prov = gson.fromJson(provClient.save(entity.getProveedor()).getBody().toString(),Proveedor.class);
-
+        try {
+            // para XtoOne
+            Proveedor prov = gson.fromJson(provClient
+                            .saveProveedor(entity.getProveedor())
+                            .getBody()
+                            .toString()
+                    , Proveedor.class);
             entity.setIdDistribProv(prov.getId());
+            entity.getProveedor().setId(prov.getId());
 
+            // para XtoMany
+            for (Articulos_Existencia a : entity.getArticulos_existencias()) {
+                Existencia existencia = gson.fromJson(provClient
+                                .saveExistencia(a.getExistencia())
+                                .getBody()
+                                .toString()
+                        , Existencia.class);
+                a.setId_existencia(existencia.getId());
+                a.getExistencia().setId(existencia.getId());
+            }
             return ResponseEntity.status(HttpStatus.OK).body(service.save(entity));
-        }catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error\"}");
         }
     }
 
     @PutMapping("/subordinada/{id}")
-    public ResponseEntity<?> updateSubordinada(@PathVariable Long id,@RequestBody Insumo entity) {
-        try{
+    public ResponseEntity<?> updateSubordinada(@PathVariable Long id, @RequestBody Insumo entity) {
+        try {
 
-            if(entity.getIdDistribProv()==0){
+            if (entity.getIdDistribProv() == 0) {
 
-                Proveedor prov = gson.fromJson(provClient.save(entity.getProveedor()).getBody().toString(),Proveedor.class);
-
+                Proveedor prov = gson.fromJson(provClient.saveProveedor(entity.getProveedor()).getBody().toString(), Proveedor.class);
                 entity.setIdDistribProv(prov.getId());
 
-            }else provClient.update(entity.getIdDistribProv(),entity.getProveedor());
+            } else provClient.updateProveedor(entity.getIdDistribProv(), entity.getProveedor());
 
 
-            return ResponseEntity.status(HttpStatus.OK).body(service.update(id,entity));
-        }catch (Exception e){
+            for (Articulos_Existencia a : entity.getArticulos_existencias()) {
+                if (a.getId_existencia() == 0) {
+                    Existencia existencia = gson.fromJson(provClient
+                                    .saveExistencia(a.getExistencia())
+                                    .getBody()
+                                    .toString()
+                            , Existencia.class);
+                    a.setId_existencia(existencia.getId());
+                    a.getExistencia().setId(existencia.getId());
+                }else provClient.updateExistencia(a.getId_existencia(),a.getExistencia());
+            }
+            return ResponseEntity.status(HttpStatus.OK).body(service.update(id, entity));
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error\"}");
         }
     }
 
-
     @PutMapping("/{id}")
-    public ResponseEntity<?> update(@PathVariable Long id,@RequestBody Insumo entity) {
-        try{
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Insumo entity) {
+        try {
 
-            Proveedor prov = gson.fromJson(provClient.getOne(entity.getIdDistribProv()).getBody().toString(),Proveedor.class);
-
+            Proveedor prov = gson.fromJson(provClient.getOneProveedor(entity.getIdDistribProv()).getBody().toString(), Proveedor.class);
             entity.setProveedor(prov);
 
-            return ResponseEntity.status(HttpStatus.OK).body(service.update(id,entity));
-        }catch (Exception e){
+            for(Articulos_Existencia a : entity.getArticulos_existencias()){
+                a.setExistencia(gson.fromJson(provClient.getOneExistencia(a.getId_existencia()).getBody().toString(),Existencia.class));
+            }
+            return ResponseEntity.status(HttpStatus.OK).body(service.update(id, entity));
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error\"}");
         }
     }
